715e1708f33a9280318836d688553965ec3ac1fd
LAPTOP-GH32M657\PhanLeHau <hau.phan0363758506@hcmut.edu.vn>
2024-09-18 20:20:49 +0700

    no message

--- SOURCE-CODE/Core/Src/exercise1.c
+++ SOURCE-CODE/Core/Src/exercise1.c
@@ -3,26 +3,19 @@
 int led_status = LED_OFF;
 
 void init_exercise1(){
-
-}
-void led_on(){
-	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
-}
-void led_off(){
-	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
 }
 void exercise1_run(){
 	switch(led_status){
 		case LED_ON:
-			led_on();
+			HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
 			if(1){
 				led_status = LED_OFF;
 			}
 			break;
 		case LED_OFF:
-			led_off();
+			HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
 			if(1){
 				led_status = LED_ON;
 			}
