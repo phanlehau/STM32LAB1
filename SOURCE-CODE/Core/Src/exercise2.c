7f8746cb5b30bde9770e4838a4626198d79f4f89
LAPTOP-GH32M657\PhanLeHau <hau.phan0363758506@hcmut.edu.vn>
2024-10-17 21:53:04 +0700

    no message

--- SOURCE-CODE/Core/Src/exercise2.c
+++ SOURCE-CODE/Core/Src/exercise2.c
@@ -6,49 +6,10 @@
   int led_status = DO;
 
 void init_exercise2(){
-	    HAL_GPIO_WritePin(RED_LED_GPIO_Port, RED_LED_Pin, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(YELLOW_LED_GPIO_Port, YELLOW_LED_Pin, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_SET);
+
 
 }
 void exercise2_run(){
-    switch(status) {
-    case DO:
-  	  couter++;
-  	  if(couter>=5)
-  	  {
-  	    HAL_GPIO_WritePin(RED_LED_GPIO_Port, RED_LED_Pin, GPIO_PIN_SET);
-  		HAL_GPIO_WritePin(YELLOW_LED_GPIO_Port, YELLOW_LED_Pin, GPIO_PIN_SET);
-  		HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_RESET);
-  		status = XANH;
-  		couter = 0;
-  	  }
-  	  break;
-    case VANG:
-  	  couter++;
-  	  if(couter>=2)
-  	  {
-  	    HAL_GPIO_WritePin(RED_LED_GPIO_Port, RED_LED_Pin, GPIO_PIN_RESET);
-  		HAL_GPIO_WritePin(YELLOW_LED_GPIO_Port, YELLOW_LED_Pin, GPIO_PIN_SET);
-  		HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_SET);
-
-    		status = DO;
-    		couter = 0;
-  	  }
-    case XANH:
-  	  couter++;
-  	  if(couter>=3)
-  	  {
-  	    HAL_GPIO_WritePin(RED_LED_GPIO_Port, RED_LED_Pin, GPIO_PIN_SET);
-  		HAL_GPIO_WritePin(YELLOW_LED_GPIO_Port, YELLOW_LED_Pin, GPIO_PIN_RESET);
-  		HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_SET);
-    		status = VANG;
-    		couter = 0;
-  	  }
-  	  break;
-    default:
-  	  break;
-    }
 
 }
 
